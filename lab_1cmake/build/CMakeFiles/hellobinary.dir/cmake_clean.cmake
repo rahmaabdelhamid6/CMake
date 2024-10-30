e8611402708ec24bf1a82739668d28ba04225495
Rahma Abdelhamid <120907787+rahmaabdelhamid6@users.noreply.github.com>
2024-10-30 15:15:55 +0300

    Add files via upload

---
+++ lab_1cmake/build/CMakeFiles/hellobinary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellobinary.dir/main.cpp.o"
+  "hellobinary"
+  "hellobinary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellobinary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab_1cmake/main.cpp
@@ -0,0 +1,11 @@
+// {1-build} g++ ./main.cpp -o ./main.exe   //compiler file name -o output file name to execute   
+// {2-run}  ./main.exe
+//{3-clear}rm  filename //to remove 
+
+#include <iostream>
+
+int main ()
+{
+    std::cout << "hello" <<std::endl;
+     return 0;
+}
